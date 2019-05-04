e5b661b1cc119276d61621ae8c12263ca11b45a3
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-05-04 17:01:03 -0300

    Interface: base

---
+++ RentCar_Interface/src/com/app/myFrame.java
@@ -0,0 +1,60 @@
+package com.app;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JInternalFrame;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.Component;
+import javax.swing.Box;
+
+public class myFrame extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					myFrame frame = new myFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public myFrame() {
+		getContentPane().setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 434, 261);
+		getContentPane().add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("New tab", null, panel, null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
+	}
+}
