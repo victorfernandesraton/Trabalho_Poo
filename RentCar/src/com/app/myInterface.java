62dd795438036d9b7cf34c5a9be3ef35ade15bba
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-05-05 12:37:30 -0300

    interface: carro cadastro + geral

--- RentCar/src/com/app/myInterface.java
+++ RentCar/src/com/app/myInterface.java
@@ -3,12 +3,12 @@ package com.app;
 import javax.swing.*;
 
 public class myInterface {
-    private JTabbedPane tabbedPane1;
+    private JTabbedPane tbAluguel;
     private JPanel panel1;
-    private JPanel tb_usuário;
-    private JPanel tb_Carro;
-    private JPanel panel4;
-    private JPanel panel5;
+    private JPanel tbusuário;
+    private JPanel tbCarro;
+    private JPanel tbRent;
+    private JPanel tbGeral;
     private JButton btCancel_User;
     private JButton btRegistery_User;
     private JTextField jTextField_name;
