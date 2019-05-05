3b2816ea0329ac9b5e3061b6a227b39d8e6d4b90
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-05-05 03:23:12 -0300

    interface: criação:intelij

---
+++ RentCar/src/com/app/myInterface.java
@@ -0,0 +1,16 @@
+package com.app;
+
+import javax.swing.*;
+
+public class myInterface {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JPanel panel3;
+    private JPanel panel4;
+    private JPanel panel5;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
