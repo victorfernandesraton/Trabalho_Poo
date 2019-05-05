e9a897ab5f174af329ae8b2ecfea7eb6c49d5256
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-05-05 12:33:29 -0300

    interface: carro cadastro

--- RentCar/src/com/app/myInterface.java
+++ RentCar/src/com/app/myInterface.java
@@ -5,16 +5,30 @@ import javax.swing.*;
 public class myInterface {
     private JTabbedPane tabbedPane1;
     private JPanel panel1;
-    private JPanel panel2;
-    private JPanel panel3;
+    private JPanel tb_usuário;
+    private JPanel tb_Carro;
     private JPanel panel4;
     private JPanel panel5;
-    private JButton btCancel;
-    private JButton btRegistery;
+    private JButton btCancel_User;
+    private JButton btRegistery_User;
     private JTextField jTextField_name;
     private JTextField textField_cpf;
     private JTextField textField_mail;
     private JTextField textField_phone;
+    private JButton btRegisteryCar;
+    private JTextField textFieldColorCar;
+    private JTextField textFieldModelCar;
+    private JButton btCancelCar;
+    private JTextField textFieldPlateCar;
+    private JComboBox comboBoxYFactoryCar;
+    private JComboBox comboBoxCategoryCar;
+    private JTextField textFieldDayPriceCar;
+    private JLabel modeloLabel;
+    private JLabel placaLabel;
+    private JLabel categoriaLabel;
+    private JLabel corLabel;
+    private JLabel anoDeFabricaçãoLabel;
+    private JLabel diáriaLabel;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
