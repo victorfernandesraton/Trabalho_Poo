28aedee263d7a7cee41bd61f8817e50191e68d6b
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-05-05 12:17:57 -0300

    interface: usuário cadastro

--- RentCar/src/com/app/myInterface.java
+++ RentCar/src/com/app/myInterface.java
@@ -9,6 +9,12 @@ public class myInterface {
     private JPanel panel3;
     private JPanel panel4;
     private JPanel panel5;
+    private JButton btCancel;
+    private JButton btRegistery;
+    private JTextField jTextField_name;
+    private JTextField textField_cpf;
+    private JTextField textField_mail;
+    private JTextField textField_phone;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
